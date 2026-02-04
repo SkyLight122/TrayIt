205dce99dbdafe4a9444558a3b0917d07fbab2e3
gracey <gracebirila@gmail.com>
2026-02-04 14:17:26 +0700

    mood-mobile

--- resources/views/pages/preparation/⚡mood.blade.php
+++ resources/views/pages/preparation/⚡mood.blade.php
@@ -13,31 +13,37 @@ new class extends Component
 };
 ?>
 
-<div class="bg-[#FFF8F0] min-h-screen py-8">
-    <div class="flex items-center justify-between px-20">
-        <h1 class="text-[#88481E] font-semibold text-5xl">What are you <br> craving right now?</h1>
-        <div class="flex gap-2">
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+<div class="min-h-screen bg-[#FFF8F0] py-10 flex flex-col justify-around relative overflow-hidden">
+
+    <div class="lg:hidden absolute bottom-[-50%] left-1/2 -translate-x-1/2
+               w-[280%] h-[130%] rounded-full"
+        style="background: radial-gradient(circle, #AE693B 35%, #FFF8F0 60%);">
+    </div>
+    
+    <div class="lg:flex items-center justify-between px-15">
+        <h1 class="text-[#AE693B] font-semibold lg:text-5xl text-3xl lg:text-left text-center z-20">What are you <br> craving right now?</h1>
+        <div class="hidden sm:flex flex gap-2">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Cake.png') }}" class="w-9 h-12"/>
             </div>
             
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Bread.png') }}" class="w-12 h-12"/>
             </div>
 
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Pastry.png') }}" class="w-12 h-12"/>
             </div>
 
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Cookie.png') }}" class="w-12 h-13"/>
             </div>
 
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Dessert.png') }}" class="w-12 h-12"/>
             </div>
             
-            <div class="w-17 h-17 rounded-full border-3 border-amber-800 bg-[#FFF8F0] flex items-center justify-center">
+            <div class="w-17 h-17 rounded-full border-3 border-[#AE693B] flex items-center justify-center">
                 <img src="{{ asset('images/Savory.png') }}" class="w-12 h-12"/>
             </div>
         </div>
@@ -45,41 +51,45 @@ new class extends Component
 
     
 
-    <div class="swiper">
+    <div class="swiper w-full">
         <div class="swiper-wrapper">
             <div class="swiper-slide" data-name="Cake">
-                <img src="/images/Cake.png" class="w-70 mx-auto">
+                <img src="{{ asset('images/Cake.png') }}" class="lg:w-70 w-50 mx-auto">
             </div>
 
             <div class="swiper-slide" data-name="Bread">
-                <img src="/images/Bread.png" class="w-90 mx-auto">
+                <img src="{{ asset('images/Bread.png') }}" class="lg:w-90 w-70 mx-auto">
             </div>
 
             <div class="swiper-slide" data-name="Pastry">
-                <img src="/images/Pastry.png" class="w-85 mx-auto">
+                <img src="{{ asset('images/Pastry.png') }}" class="lg:w-85 w-65 mx-auto">
             </div>
 
             <div class="swiper-slide" data-name="Cookie">
-                <img src="/images/Cookie.png" class="w-95 mx-auto">
+                <img src="{{ asset('images/Cookie.png') }}" class="lg:w-95 w-75 mx-auto">
             </div>
 
             <div class="swiper-slide" data-name="Dessert">
-                <img src="/images/Dessert.png" class="w-90 mx-auto">
+                <img src="{{ asset('images/Dessert.png') }}" class="lg:w-90 w-70 mx-auto">
             </div>
 
             <div class="swiper-slide" data-name="Savory">
-                <img src="/images/Savory.png" class="w-100 mx-auto">
+                <img src="{{ asset('images/Savory.png') }}" class="lg:w-100 w-80 mx-auto">
             </div>
         </div>
     </div>
 
-    <div class="flex justify-center font-semibold">
-        <button id="categoryButton" class="mt-6 w-40 px-6 py-2 border-2 rounded-lg text-center text-[#88481E] border-[#88481E] hover:bg-[#88481E] hover:text-[#FFF8F0]">Cake</button>
+    <div class="flex justify-center font-semibold z-20">
+        <button id="categoryButton" class="mt-6 w-40 px-6 py-2 border-2 rounded-lg text-center 
+        text-[#FFF8F0] border-[#FFF8F0] hover:bg-[#FFF8F0] hover:text-[#AE693B]
+        lg:text-[#AE693B] lg:border-[#AE693B] lg:hover:bg-[#AE693B] lg:hover:text-[#FFF8F0]">Cake</button>
     </div>
 
 </div>
 
 <style>
+
+
     .swiper-slide-active {
   filter: blur(0x);
 }
@@ -110,10 +120,18 @@ new class extends Component
         //untuk ubah bentuk cursor jadi bentuk tangan yang genggam
         centeredSlides: true,
         //jadi slide yang aktif adalah slide yang ditengah
-        slidesPerView: "2",
+        slidesPerView: "1",
         //ini buat ngatur berapa tampilan yang muncul dihalaman
         slideToClickedSlide: true,
         //fungsinya untuk animasi tinggal klik aja 
+        slidesPerView: 1, // DEFAULT HP
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            }
+        },
+        
         coverflowEffect: {
             rotate: 0,
             stretch: 80,
@@ -128,8 +146,11 @@ new class extends Component
             //buat blur pada slide yang tidak aktif
             scale: 0.85,
             //ini buat jadiin slide yang tidak aktif menjadi lebih kecil
+
         },
         loop: true,
+
+
 
     on: {
         slideChange: function () {
