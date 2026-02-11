94a505b5337d0cfe6b6ccfc4a13d647cfd2f60ba
SkyLight122 <vincentnicola32@gmail.com>
2026-02-11 17:05:14 +0700

    function data analisis owner bakery

--- resources/views/pages/owner_bakery/⚡dashboard.blade.php
+++ resources/views/pages/owner_bakery/⚡dashboard.blade.php
@@ -3,6 +3,8 @@
 use Livewire\Component;
 use Livewire\Attributes\Computed;
 use App\Models\Product;
+use App\Models\Order;
+use App\Models\User;
 
 new class extends Component
 {
@@ -15,6 +17,23 @@ new class extends Component
     #[Computed]
     public function total_menu(){
         return Product::where('bakery_id', $this->bakery_id)->count();
+    }
+
+    #[Computed]
+    public function total_customer(){
+        return Order::where('bakery_id', $this->bakery_id)->distinct()->count('user_id');
+    }
+
+    #[Computed]
+    public function customer_this_day(){
+        return User::whereHas('orders', function ($q){
+            $q -> where('bakery_id', $this->bakery_id)->whereDate('created_at', today());
+        })->get();
+    }
+
+    #[Computed]
+    public function best_seller_products(){
+        return Product::where('bakery_id', $this->bakery_id)->withSum('order_products', 'quantity')->orderByDesc('order_products_sum_quantity')->take(5)->get();
     }
 };
 ?>
