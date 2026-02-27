616ffbda416e2ee0484a138f7944e75bde19a732
SkyLight122 <vincentnicola32@gmail.com>
2026-02-27 09:37:49 +0700

    middleware route update

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,17 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Route::middleware(['auth', 'customer'])->group(function(){
-//     Route::livewire('/product', 'pages::bakery.product')->name('product');
-//     Route::livewire('/history', 'pages::bakery.history')->name('history');
-//     Route::livewire('/aboutus', 'pages::bakery.about_us')->name('aboutus');
-//     Route::livewire('/detail', 'pages::bakery.detail_product') -> name('detail');
+//     Route::livewire('/product/{id}', 'pages::bakery.product')->name('product');
+//     Route::livewire('/history/{id}', 'pages::bakery.history')->name('history');
+//     Route::livewire('/aboutus/{id}', 'pages::bakery.about_us')->name('aboutus');
+//     Route::livewire('/detail/{id}', 'pages::bakery.detail_product') -> name('detail');
 // });
 // Route::middleware(['auth', 'bakeryowner'])->group(function(){
-//     Route::livewire('/dashboard', 'pages::owner_bakery.dashboard')->name('dashboard');
-//     Route::livewire('/orders', 'pages::owner_bakery.orders')->name('orders');
-//     Route::livewire('/customers', 'pages::owner_bakery.orders')->name('customers');
+//     Route::livewire('/dashboard/{id}', 'pages::owner_bakery.dashboard')->name('dashboard');
+//     Route::livewire('/orders/{id}', 'pages::owner_bakery.orders')->name('orders');
+//     Route::livewire('/customers/{id}', 'pages::owner_bakery.orders')->name('customers');
+//     Route::livewire('/hpp/{id}', 'pages::owner_bakery.hpp') -> name('hpp');
+
 // });
 // Route::middleware(['auth', 'admin'])->group(function(){
 
@@ -24,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 //     Route::livewire('/mood', 'pages::preparation.mood')->name('mood');
 //     Route::livewire('/portal', 'pages::preparation.portal')->name('portal');
 //     Route::livewire('/createbakery', 'pages::admin.createbakery')->name('createbakery');
+//     Route::livewire('/logout', 'pages::owner_bakery.hpp') -> name('logout');
 // });
 // Route::middleware(['guest'])->group(function(){
 //     Route::livewire('/login', 'pages::auth.login')->name('login');
@@ -45,8 +48,5 @@ Route::livewire('/login', 'pages::auth.login')->name('login');
 Route::livewire('/register', 'pages::auth.register')->name('register');
 Route::livewire('/', 'pages::guest.landing_page');
 Route::livewire('/sub', 'pages::guest.slider')->name('sub');
-
-
-Route::livewire('/products', 'pages::bakery.product') -> name('products');
-Route::livewire('/hpp', 'pages::owner_bakery.hpp') -> name('hpp');
 Route::livewire('/logout', 'pages::owner_bakery.hpp') -> name('logout');
+Route::livewire('/hpp/{id}', 'pages::owner_bakery.hpp') -> name('hpp');
